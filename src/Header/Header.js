885b547dc70b16daefc41b89f9593156cf74a883
Jared Kozar <jared.rkozar@gmail.com>
2022-06-04 20:47:34 -0400

    added memoji icon

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,7 +6,7 @@ function Header() {
 <div class="flex auto py-16 flex-col space-y-10">
  
   <div class="relative text-6xl font-bold dark:text-white">Hi, I'm Jared Kozar.</div>
-  <div class="relative text-4xl dark:text-white mr-36">I am a junior majoring in computer science at the University of Hartford. I enjoy developing apps that help users, especially those who are visually impaired, improve their productivity on their iPhone, iPad, Mac, and Apple Watch. </div>
+  <div class="relative text-4xl dark:text-white mr-32">I am a junior majoring in computer science at the University of Hartford. I enjoy developing apps that help users, especially those who are visually impaired, improve their productivity on their iPhone, iPad, Mac, and Apple Watch. </div>
 
 
 </div>
--- src/NavBar/NavBar.js
+++ src/NavBar/NavBar.js
@@ -1,20 +1,19 @@
 import React from "react";
-import { ReactComponent as JKIcon } from  "../Images/JKIcon.svg";
-import NavBarItem from '../Components/NavBarItem';
 import { ReactComponent as GithubIcon } from  "../Images/GithubIcon.svg";
 import { ReactComponent as EmailIcon } from  "../Images/EmailIcon.svg";
 import { ReactComponent as LinkedInIcon } from  "../Images/LinkedInIcon.svg";
 import { ReactComponent as ResumeIcon } from "../Images/resumeIcon.svg";
+import MemojiIcon from  "../Images/Memoji.png";
 
 function NavBar() {
   return (
-    <nav className="bg-inherit text-inherit flex justify-center items-center h-20">
-        <div className=" w-full flex flex-wrap bg-transparent justify-between px-6">
+    <nav className="bg-inherit text-inherit flex items-center h-20 relative top-5">
+        <div className="w-full flex flex-wrap bg-transparent justify-between px-6">
         
-            <div class="flex items-center relative gap-3">
-                <div class="text-2xl font-bold gap-4 flex rounded-md relative bg-transparent">
-                   <JKIcon/>
-                   <h1>Jared Kozar</h1>
+            <div class="relative gap-3">
+                <div class="text-2xl font-bold gap-4 flex rounded-md relative bg-transparent space-x-15 flex items-center">
+                   <img class="object-cover h-14 w-14 rounded-md" src={MemojiIcon}></img>
+                   <h1 class="hidden md:block lg:block">Jared Kozar</h1>
                 </div>
                 </div>
         
