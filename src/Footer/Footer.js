887c1757b8aa74366bf9f3d36441bfbf1db02872
Jared Kozar <jared.rkozar@gmail.com>
2022-06-06 22:32:41 -0400

    increased footer size

--- src/Footer/Footer.js
+++ src/Footer/Footer.js
@@ -4,9 +4,10 @@ function Footer() {
           <h2 className="text-4xl font-bold"> Contact</h2>
   
           <h3 className="text-2xl font-semibold py-2"> Feel free to contact me at jared.rkozar@gmail.com or connect with me on LinkedIn if you want me to help you with any projects</h3>
-          
-          <h3 class="grid place-items-center">Jared Kozar, 2022</h3>
-          <h3 class="grid place-items-center">Made with React.js and Tailwind CSS</h3>
+          <div className="py-6 grid place-items-center text-lg font-medium">
+            <h3>Jared Kozar, 2022</h3>
+            <h3>Made with React and Tailwind CSS</h3>
+          </div>
       </div>
     );
   }
