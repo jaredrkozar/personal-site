0677e7f7587e61d325814cad695fcfc72eca3418
Jared Kozar <jared.rkozar@gmail.com>
2023-03-13 00:13:06 -0400

    beginning of redesign

--- src/App.js
+++ src/App.js
@@ -9,17 +9,7 @@ import { DetailStyle } from "./Components/TextStyles.js";
 function App() {
   return (
     <div className="w-full bg-inherit">
-      <div className="flex flex-col fixed w-full bg-inherit z-10">
-        <NavBar showBackButton={false}/>
-      </div>
-        <div className="ml-5 relative mr-5 space-y-10 py-20">
-          <Header/>
-          <AboutMe/>
-          <IOSApps/>
-          <OtherApps/>
-          <DetailStyle mainText="Contact" detailText="Feel free to contact me at jared.rkozar@gmail.com or connect with me on LinkedIn if you want me to help you with any projects" isItalic={false}></DetailStyle>
-          <Footer/>
-        </div>
+      <h1>Redesign</h1>
      </div>
   );
 }
