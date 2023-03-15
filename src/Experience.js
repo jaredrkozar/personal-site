f67da99b0d23aaeea45b60fd07a139f93c314a1a
Jared Kozar <jared.rkozar@gmail.com>
2023-03-15 17:14:19 -0400

    fixed timeline color

--- src/Experience.js
+++ src/Experience.js
@@ -23,6 +23,17 @@ function Experience() {
               "Leveraged AWS Lambda to migrate Node.js backend service to a cloud-based architecture, removing the need to pull from a developer's local machine.",
               "Designed a UI using CSS and React so engineers can easily debug user accounts without having to run a script."
           ]
+          },
+          {
+            positionName: "Technology Leadership Development Program Intern",
+            companyName: "Travelers Insurance",
+            startDate: "June 2022",
+            endDate: "August 2022",
+            responsibilities: [
+              "Wrote a script using Node and Express to collect data from 4 internal services for debugging user accounts.",
+              "Leveraged AWS Lambda to migrate Node.js backend service to a cloud-based architecture, removing the need to pull from a developer's local machine.",
+              "Designed a UI using CSS and React so engineers can easily debug user accounts without having to run a script."
+          ]
           }
        ]
 
@@ -30,17 +41,19 @@ function Experience() {
         <div className="relative">
             <HeaderTextStyle title="Experience" bgStyle="bg-blue-600 dark:bg-blue-500" description="Here are some of the companies I have worked at in the past: "></HeaderTextStyle>
 
-            <ol class="relative border-l-8 border-gray-200 dark:border-gray-700">                  
+            <ol class="relative top-6 border-l-8 border-gray-200 dark:border-gray-700 dark:border-green-700">                  
     {experienceList.map((number) =>
-        <li class="mb-10 ml-4">
-        <span class="absolute flex items-center justify-center w-6 h-6 bg-orange-300 first:bg-yellow-300 dark:bg-green-300 rounded-full -left-4 ring-2 ring-gray dark:ring-white"></span>
-        <h1 className="font-bold text-black dark:text-white">{number.positionName}</h1>
-                <h1 className="font-semibold italic dark:text-white">{number.companyName}</h1>
-                <h1 className="text-black dark:text-white">{number.startDate + "-" + number.endDate}</h1>
+        <div className="group">
+            <span class="absolute flex items-center justify-center w-6 h-6 bg-green-500 last:bg-black rounded-full -left-4 ring-2 group-first:dark:ring-white dark:ring-green-500"></span>
+            <li class="mb-10 ml-4 bg-slate-300 dark:bg-slate-700 first:bg-white p-4 rounded-lg w-1/2 dark:text-white text-black ">
+                <h1 className="font-bold dark:text-white text-2xl">{number.positionName}</h1>
+                <h1 className="font-semibold italic text-xl">{number.companyName}</h1>
+                <h1 className="text-xl">{number.startDate + "-" + number.endDate}</h1>
                 {number.responsibilities.map((responsibility) =>
-                <li className="list-inside list-disc text-black dark:text-white">{responsibility}</li>
+                    <li className="list-inside list-disc text-xl">{responsibility}</li>
                 )}
-    </li>
+            </li>
+            </div>
     )}
 </ol>
         </div>
