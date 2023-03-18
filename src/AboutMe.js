60d6f68dc06edf038c582e9fc69360352ad94365
Jared Kozar <jared.rkozar@gmail.com>
2023-03-18 11:45:20 -0400

    changed margins

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -2,7 +2,7 @@ function AboutMe() {
   return (
     <div className="ml-5 relative w-fit h-fit lg:mr-5 sm:mr-2 top-60 pb-60">
         <div className="flex flex-row h-fit">
-            <div className="h-full relative mr-20 space-y-10 flex flex-col text-black dark:text-black">
+            <div className="h-full relative md:mr-60 space-y-10 flex flex-col text-black dark:text-black">
                 <h1 className="text-7xl font-semibold dark:text-white">Hi! I'm Jared Kozar</h1>
 
                 <h1 className="text-4xl font-medium dark:text-slate-300">I'm a software enigineer who is passionate about solving difficult problems with code. I have over 2 years of experience with React and Swift to develop applications for web and mobile devices respectively.</h1>
