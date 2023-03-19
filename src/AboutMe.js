63a433405aa4b5b7a5b0cb758dc1d9d58e78d887
Jared Kozar <jared.rkozar@gmail.com>
2023-03-19 03:17:51 -0400

    changed font weight

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -7,7 +7,7 @@ function AboutMe() {
 
                 <h1 className="text-4xl font-medium dark:text-slate-300">I'm a software engineer who is passionate and motivated about solving challenging problems with code. I have 2+ years of experience using React and Swift to develop applications for web and iOS devices.</h1>
 
-                <h1 className="text-4xl font-medium dark:text-slate-300">Previously, I worked at Travelers Insurance as a Technology Leadership Development Program intern writing full stack applications using React and Node.js </h1>
+                <h1 className="text-4xl font-medium dark:text-slate-300">Previously, I worked at Travelers Insurance as a Technology Leadership Development Program Intern writing full stack applications using React and Node.js </h1>
             </div>
         </div>
      </div>
--- src/Contact.js
+++ src/Contact.js
@@ -5,7 +5,7 @@ function Contact() {
         <div className="">
             <HeaderTextStyle title="Contact" bgStyle="bg-navyBlue-400 dark:bg-navyBlue-500" description=""></HeaderTextStyle>
         
-            <h1 className="text-3xl font-medium dark:text-white">If you would like to collaborate on a project or have any questions, feel free to email me at <a className="text-navyBlue-400 dark:text-navyBlue-500 underline" href = "mailto:jared.rkozar@gmail.com">jared.rkozar@gmail.com</a> or connect on LinkedIn</h1>
+            <h1 className="text-3xl font-regular dark:text-white">If you would like to collaborate on a project or have any questions, feel free to email me at <a className="text-navyBlue-400 dark:text-navyBlue-500 underline" href = "mailto:jared.rkozar@gmail.com">jared.rkozar@gmail.com</a> or connect on LinkedIn</h1>
         </div>
     )
 }
