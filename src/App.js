e8ae66fb98d35c2905864b7f6f5295120b5feb0c
Jared Kozar <jared.rkozar@gmail.com>
2023-08-17 03:11:56 -0400

    added education

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import IOSApps from "./apps/iOSApps";
 import WebApps from "./apps/WebApps";
 import Skills from "./Skills";
 import Experience from "./Experience";
+import Education from "./Education";
 import Contact from "./Contact";
 
 function App() {
@@ -18,8 +19,9 @@ function App() {
       <div className="ml-5 relative mr-5 space-y-10 py-20">
       <Skills/>
       <Experience/>
-      <WebApps/>
       <IOSApps/>
+      <WebApps/>
+      <Education/>
       <Contact/>
       </div>
       <div className="w-full justify-center items-center flex flex-col pb-8">
---
+++ src/Education.js
@@ -0,0 +1,13 @@
+import { HeaderTextStyle } from "./components/TextStyles"
+
+function Education() {
+    return (
+        <div className="">
+            <HeaderTextStyle title="Education" bgStyle="bg-red-400 dark:bg-red-600">
+                <h1 className="md:text-3xl text-2xl font-regular text-black dark:text-white">I graduated from the <a className="text-red-400 dark:text-red-600 underline" href = "https://www.hartford.edu"  aria-label="University of Hartford web site" target="_blank" rel="noreferrer">University of Hartford</a> in 2023, where I majored in Computer Science and Web Design, with a 4.0 GPA (summa cum laude).</h1>
+            </HeaderTextStyle>
+        </div>
+    )
+}
+
+export default Education; 
